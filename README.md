c29c06f67ebc2e52483ca0407a9abdd0cf75fe81
Harshith Ashok <50227707+harshith-ashok@users.noreply.github.com>
2026-03-06 19:51:53 +0530

    v1

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ---
 
 manage your files and folders and search them all automatically with AI
+
+- Send request to `chat`
+
+```bash
+curl -X POST http://localhost:8122 \
+  -H "Content-Type: application/json" \
+  -d '{"message":"Explain in one sentence what the sun is"}'
+```
--- app.py
+++ app.py
@@ -1,15 +1,44 @@
-import os
-
 from fastapi import FastAPI
-import uvicorn
+from pydantic import BaseModel
+import ollama
+import json
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Chat(BaseModel):
+    message: str
 
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8122))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+
+SYSTEM_PROMPT = """
+Convert natural language requests into Unix shell commands.
+
+Return ONLY JSON.
+
+Format:
+{
+ "command": "<shell command>"
+}
+
+No explanations.
+"""
+
+
+@app.post("/chat")
+def chat(data: Chat):
+    res = ollama.chat(
+        model="qwen2.5:3b",
+        messages=[
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": data.message},
+        ],
+    )
+
+    output = res["message"]["content"]
+
+    try:
+        command = json.loads(output)["command"]
+    except:
+        command = output.split("\n")[0]
+
+    return {"command": command.strip()}
